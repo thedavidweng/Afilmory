ca032efccc0634ab0638935d167cc203f2fb31a7
Davy <95214375+thedavidweng@users.noreply.github.com>
2025-06-14 18:18:59 -0700

    Update env.ts

--- env.ts
+++ env.ts
@@ -5,17 +5,23 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
+    S3_ACCESS_KEY_ID: z.string().optional(),
+    S3_SECRET_ACCESS_KEY: z.string().optional(),
+    S3_ENDPOINT: z.string().optional(),
+    S3_BUCKET_NAME: z.string().optional(),
+    S3_PREFIX: z.string().optional(),
+    S3_CUSTOM_DOMAIN: z.string().optional(),
     S3_REGION: z.string().default('us-east-1'),
-    S3_ACCESS_KEY_ID: z.string().min(1).optional(),
-    S3_SECRET_ACCESS_KEY: z.string().min(1).optional(),
-    S3_ENDPOINT: z
-      .string()
-      .default('https://s3.us-east-1.amazonaws.com')
-      .optional(),
-    S3_BUCKET_NAME: z.string().min(1).optional(),
-    S3_PREFIX: z.string().default('').optional(),
-    S3_CUSTOM_DOMAIN: z.string().default('').optional(),
+    GITHUB_TOKEN: z.string().optional(),
+    GITHUB_OWNER: z.string().optional(),
+    GITHUB_REPO: z.string().optional(),
+    GITHUB_BRANCH: z.string().optional(),
+    GITHUB_PATH: z.string().optional(),
+    GITHUB_USE_RAW_URL: z.string().optional(),
   },
+  client: {},
+  clientPrefix: 'NEXT_PUBLIC_',
   runtimeEnv: process.env,
   isServer: typeof window === 'undefined',
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 })
